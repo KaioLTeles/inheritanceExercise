84dbcb615f83b434d5318848e027fadfbcc862d0
Kaio Teles <kaiolunateles@gmail.com>
2019-08-08 21:40:02 -0300

    Inicial commit.

---
+++ inheritance-csharpNew/Entities/ImportedProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using inheritance_csharpNew.Entities;
+using System.Globalization;
+
+namespace inheritance_csharpNew.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct( string name, double price,double customFee)
+            : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+
+        public double totalPrice()
+        {
+            return Price + CustomFee;
+        }
+
+        public override string priceTag()
+        {
+            return Name
+                    + " $ " 
+                    + totalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                    + "(Custom fee: $ "
+                    + ")";
+        }
+
+
+    }
+}
